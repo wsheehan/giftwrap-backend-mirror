5e92278f8493c546c4b96676cc9ca975a77c34ac
Will Sheehan <wsheehan@bates.edu>
2016-07-26 15:03:33 -0400

    Twilio use real number/creds

--- app/controllers/text_campaigns_controller.rb
+++ app/controllers/text_campaigns_controller.rb
@@ -5,11 +5,13 @@ class TextCampaignsController < ApplicationController
   def create
     @client = Twilio::REST::Client.new
     @client.messages.create(
-      from: '+15005550006', # This is a test number that will not actually send a text
-      to: params[:donor],
-      body: params[:body]
+      #from: '+15005550006', # This is a test number that will not actually send a text
+      from: '+18023597135',
+      to: params[:text_campaign][:donor_number], # For now this only works with my number 16034430335
+      body: params[:text_campaign][:body]
     )
     # Also Save into database
+    render json: {"Text Status": "Sent"}
   end
 
   def show
