e41f804a49281d556b4e3a829cf2ba825e984092
Will Sheehan <wsheehan@bates.edu>
2016-07-29 14:04:23 -0400

    Demo for Greg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :campaigns do
+    get 'demos/new'
+  end
+
+  namespace :campaigns do
+    get 'demos/create'
+  end
+
 namespace :api do
   namespace :v1 do
     resources :gifts, only: [:create]
@@ -22,6 +30,7 @@ end
   namespace :campaigns do
     resources :texts, only: [:index, :show, :create]
     resources :emails, only: [:index, :show, :create]
+    resources :demos, only: [:new, :create]
   end
 
 end
