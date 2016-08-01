ef0661e4b99f973333dbecdcddd48777028dabce
Will Sheehan <wsheehan@bates.edu>
2016-08-01 16:56:46 -0400

    Autoload lib classes

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Giving
 
     config.api_only = true
 
+    config.autoload_paths << Rails.root.join('lib')
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*' # Will be an array of school domains
