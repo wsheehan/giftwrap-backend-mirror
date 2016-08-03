70759fcbd092295ddee32e2ac93504fe4cc61361
Benjamin Sheehan <ben.sheehan@subtext.org>
2016-08-03 14:54:32 -0400

    Fixing other migrations

--- db/migrate/20160728133438_remove_form_from_school_and_add_to_client.rb
+++ db/migrate/20160728133438_remove_form_from_school_and_add_to_client.rb
@@ -1,6 +1,6 @@
 class RemoveFormFromSchoolAndAddToClient < ActiveRecord::Migration[5.0]
   def change
     remove_column :forms, :school_id
-    add_reference :forms, :client
+    add_reference :forms, :client, index: true, foreign_key: true
   end
 end
