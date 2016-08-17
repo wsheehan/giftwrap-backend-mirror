b426d6c47cb11b0d03df0bbc81e7a332045d243c
Will Sheehan <wsheehan@bates.edu>
2016-08-17 12:49:58 -0400

    If no recurring

--- lib/campaign/validator.rb
+++ lib/campaign/validator.rb
@@ -3,7 +3,7 @@ module Campaign::Validator
   def validate_response body
     parsed = body.split(",").map(&:strip)
     if parsed[0].match(/^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/)
-      ["monthly", "quarterly", "yearly"].include?(parsed[1].downcase) ? [parsed[0], parsed[1].downcase] : [parsed[0]]
+      ["monthly", "quarterly", "yearly"].include?(parsed[1].try(:downcase)) ? [parsed[0], parsed[1].downcase] : [parsed[0]]
     else
       false
     end
