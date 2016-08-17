eda9000917088a01dad6bf045685bb2a53dfaf9e
Will Sheehan <wsheehan@bates.edu>
2016-08-17 11:58:58 -0400

    Change instance var

--- lib/campaign/validator.rb
+++ lib/campaign/validator.rb
@@ -1,11 +1,9 @@
 module Campaign::Validator
 
-  @freq = ["monthly", "quarterly", "yearly"]
-
   def validate_response body
     parsed = body.split(",").map(&:strip)
     if parsed[0].match(/^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/)
-      @freq.include?(parsed[1].downcase) ? [parsed[0], parsed[1].downcase] : [parsed[0]]
+      ["monthly", "quarterly", "yearly"].include?(parsed[1].downcase) ? [parsed[0], parsed[1].downcase] : [parsed[0]]
     else
       false
     end
