793129a4e50c77f45ebff32d2b09bbe4a415342c
Will Sheehan <wsheehan@bates.edu>
2016-08-17 11:19:57 -0400

    Text Validator/Parser

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -1,13 +1,18 @@
 class EmailProcessor
+  include Campaign::Validator
+
   def initialize email
     @email = email
   end
 
   def process
-    @donor = Donor.find_by email: @email.from[:email]
-    @gift = @donor.gifts.build(total: @email.body)
-    if @gift.save
-      CampaignMailer.successful_gift(@donor, @email.from[:subject]).deliver_now
+    validated = validate_response @email.body
+    if validated
+      @donor = Donor.find_by email: @email.from[:email]
+      @gift = @donor.gifts.build(total: validated[0], gift_frequency: validated[1])
+      if @gift.save
+        CampaignMailer.successful_gift(@donor, @email.from[:subject]).deliver_now
+      end
     else
       CampaignMailer.unsuccessful_gift(@donor, @email.from[:subject]).deliver_now
     end
