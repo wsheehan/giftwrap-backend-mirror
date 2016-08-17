219d899152ea7f4a18819615da3d304284810085
Will Sheehan <wsheehan@bates.edu>
2016-08-17 12:47:53 -0400

    Subcription handling

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -9,12 +9,19 @@ class EmailProcessor
     validated = validate_response @email.body
     if validated
       @donor = Donor.find_by email: @email.from[:email]
-      @gift = @donor.gifts.build(total: validated[0], gift_frequency: validated[1])
+      @gift = @donor.gifts.build(total: validated[0])
       if @gift.save
+        handle_subscription if validated[1]
         CampaignMailer.successful_gift(@donor, @email.from[:subject]).deliver_now
       end
     else
       CampaignMailer.unsuccessful_gift(@donor, @email.from[:subject]).deliver_now
     end
+  end
+
+  def handle_subscription
+    @subscription = Subscription.find_by frequency: validated[1]
+    @subscription.donors << @donor
+    @donor.update_attributes(subscription_start: Date.today, subscription_total: validated[0])
   end
 end
