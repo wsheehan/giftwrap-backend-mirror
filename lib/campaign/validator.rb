23847a1bc3e4ae28e7a366880171491c5a6bab9d
Will Sheehan <wsheehan@bates.edu>
2016-09-10 13:06:32 -0400

    Validate and Error handle text responses

--- lib/campaign/validator.rb
+++ lib/campaign/validator.rb
@@ -2,6 +2,7 @@ module Campaign::Validator
 
   def validate_response body
     parsed = body.split(",").map(&:strip)
+    parsed[0] = parsed[0][1..-1] if parsed[0][0] == "$"
     if parsed[0].match(/^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/)
       ["monthly", "quarterly", "yearly"].include?(parsed[1].try(:downcase)) ? [parsed[0], parsed[1].downcase] : [parsed[0]]
     else
