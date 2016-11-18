d10711649b1cbfc63d22af980410a1af5e74b510
Will Sheehan <wsheehan@bates.edu>
2016-11-17 19:25:46 -0500

    Change email title to subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116183125) do
+ActiveRecord::Schema.define(version: 20161118001925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "campaign_emails", force: :cascade do |t|
     t.integer  "campaign_id"
-    t.string   "title"
+    t.string   "subject"
     t.string   "body"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
