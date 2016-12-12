9653bcb9d83b0b3e8cf6be653aedc5de541010ea
Will Sheehan <wsheehan@bates.edu>
2016-12-12 15:33:51 -0500

    Add Header Image to Campaign::Email

--- app/uploaders/email_header_uploader.rb
+++ app/uploaders/email_header_uploader.rb
@@ -4,9 +4,12 @@ class EmailHeaderUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # Store in S3 on production
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -27,11 +30,6 @@ class EmailHeaderUploader < CarrierWave::Uploader::Base
   #
   # def scale(width, height)
   #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
