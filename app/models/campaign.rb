5d84d9ace7eeb897366c453135fe78710302395e
Will Sheehan <wsheehan@bates.edu>
2017-01-16 14:53:13 -0500

    Get conversion and total raised statistics

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,6 +4,8 @@ class Campaign < ApplicationRecord
   belongs_to :user
   belongs_to :client
   belongs_to :donor_list
+
+  has_many :gifts
   has_many :campaign_conversions, class_name: "Metric::CampaignConversion"
 
   has_one :text, class_name: 'Campaign::Text'
@@ -11,6 +13,24 @@ class Campaign < ApplicationRecord
 
   def donor_list_donors
     donor_list.donors
+  end
+
+  def campaigns_sent
+    campaign_conversions.size
+  end
+
+  def campaigns_converted
+    campaigns_sent - campaign_conversions.where(gift_id: nil).length
+  end
+
+  def total_raised
+    @total = 0
+    gifts.each { |x| @total += x.total.to_f }
+    @total
+  end
+
+  def new_subscriptions
+    campaign_conversions.where(subscription: true).length
   end
 
 end
