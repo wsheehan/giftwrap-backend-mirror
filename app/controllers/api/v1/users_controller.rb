3e5b3df3ef31e7508633b492459d82cbba4f81e4
Will Sheehan <wsheehan@bates.edu>
2017-02-05 14:47:18 -0500

    Need to convert param to int in conditional

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    if @user_creds["user_id"] == params[:id]
+    if @user_creds["user_id"] == params["id"].to_i
       @user = User.find(@user_creds["user_id"])
       render json: { "user": UserSerializer.new(@user) }
     else
