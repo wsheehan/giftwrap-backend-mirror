e65154431332b1ad6c62723939ca4d1005c1b4a0
Will Sheehan <wsheehan@bates.edu>
2017-02-07 14:45:21 -0500

    Create and Update actions on users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,30 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    if @user_creds["user_id"] == params["id"].to_i
+    if correct_user?
       @user = User.find(@user_creds["user_id"])
       render json: { "user": UserSerializer.new(@user) }
     else
       render json: {}, status: :forbidden
     end
   end
+
+  def update
+    if correct_user?
+      @user = User.find(@user_creds["user_id"])
+      @user.update_attributes(user_params)
+      render json: {}, status: :created
+    else
+      render json: {}, status: :forbidden
+    end
+  end
+
+  private
+
+    def user_params
+      params.require("users").permit(:avatar, :first_name, :last_name, :email)
+    end
+
+    def correct_user?
+      @user_creds["user_id"] == params["id"].to_i
+    end
 end
